5f745fed7013a1bb1d49834f5fb2f0bb66c23673
jeffrey <jeffrey@techfin.ai>
2020-09-14 12:19:28 +0800

    deploy try

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,12 +4,15 @@ export default {
   ** Nuxt rendering mode
   ** See https://nuxtjs.org/api/configuration-mode
   */
-  mode: 'universal',
+  mode: 'spa',
+  router: {
+    base: '/jeffreythecoder.github.io/'
+  },
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'server',
+  target: 'static',
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
