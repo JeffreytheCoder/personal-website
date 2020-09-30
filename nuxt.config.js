edc62b205766177090e2b5942f0db15d66e19042
jeffrey <jeffrey@techfin.ai>
2020-09-30 14:42:32 +0800

    edit

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: 'Jeffrey Zepeng Yu',
+    title: 'Zepeng Yu',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
